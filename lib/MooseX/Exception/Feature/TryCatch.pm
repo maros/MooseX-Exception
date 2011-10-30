b8a65b5cba3dd17d352accf7523b32ce4a42741a
Maros Kollar <maros@k-1.com>
2011-10-30 15:45:47 +0100

    Feature try-catch working

--- lib/MooseX/Exception/Feature/TryCatch.pm
+++ lib/MooseX/Exception/Feature/TryCatch.pm
@@ -4,25 +4,23 @@ package MooseX::Exception::Feature::TryCatch;
 use utf8;
 
 use Moose::Exporter;
-#use MooseX::Exception;
 use MooseX::Exception::TryCatch;
+
+use Scalar::Util qw(blessed);
 
 Moose::Exporter->setup_import_methods(
     as_is       => [qw(try catch where finally)],
 );
-#
-#exception 'MooseX::Exception::Exception::TryCatch' => sub{
-#    extends('Internal MooseX::Exception::Base');
-#    description('Wrong TryCatch usage');
-#};
 
 sub try(&;@) {
-    my ($code,@parts) = @_;
+    my ($try,@parts) = @_;
     
+    # Store requested context
     my $wantarray = wantarray;
     
-    my ( @catch_where, $catch_all, $finally, $where_condition );
+    my ( @catch_where, $catch_all, @finally, $current_where_condition );
     
+    # Get all defined parts
     foreach my $part (@parts) {
         next 
             unless $part;
@@ -30,39 +28,77 @@ sub try(&;@) {
         my $reference = ref($part);
         
         if ( $reference eq 'MooseX::Exception::Feature::TryCatch::Where' ) {
-            if (defined $where_condition) {
-                MooseX::Exception::Exception::TryCatch->throw('Detected unbalanced where/catch blocks');
+            if (defined $current_where_condition) {
+                MooseX::Exception::TryCatch->throw('Detected unbalanced where/catch blocks');
             }
-            $where_condition = $part;
+            $current_where_condition = $$part;
         } elsif ( $reference eq 'MooseX::Exception::Feature::TryCatch::Catch' ) {
-            if (defined $where_condition) {
-                push(@catch_where,[ $where_condition,$part ]);
-                undef $where_condition;
+            if (defined $current_where_condition) {
+                push(@catch_where,[ $current_where_condition,$$part ]);
+                undef $current_where_condition;
             } elsif (defined $catch_all) {
-                MooseX::Exception::Exception::TryCatch->throw('Detected multiple catch blocks');
+                MooseX::Exception::TryCatch->throw('Detected multiple catch blocks');
             } else {
-                $catch_all = $part
+                $catch_all = $$part
             }
         } elsif ( $reference eq 'MooseX::Exception::Feature::TryCatch::Finally' ) {
-            if (defined $finally) {
-                MooseX::Exception::Exception::TryCatch->throw('Detected multiple finally blocks');
-            }
-            warn('SET FINALLY');
-            $finally = $part;
+           push(@finally,$$part);
         } else {
-            MooseX::Exception::Exception::TryCatch->throw("Unknown part given '$reference'");
+            MooseX::Exception::TryCatch->throw("Unknown part given '$reference'");
         }
     }
     
-    use Data::Dumper;
-    warn Dumper {
-        try         => $code,
-        catch_where => \@catch_where,
-        catch_all   => $catch_all,
-        finally     => $finally,
-    };
+    # Store previous error
+    my $previous_error = $@;
     
-    warn 'CALLED TRY WITH '."\n".join "\n",@_;
+    my ( @ret, $failed, $error );
+    
+    {
+        local $@;
+        
+        $failed = not eval {
+            $@ = $previous_error;
+            
+            # evaluate the try block in the correct context
+            if ( $wantarray ) {
+                @ret = $try->();
+            } elsif ( defined $wantarray ) {
+                $ret[0] = $try->();
+            } else {
+                $try->();
+            };
+            
+            return 1; # properly set $fail to false
+        };
+        $error = $@;
+    }
+    
+    # Handle error
+    if ($failed) {
+        unless (ref $error)  {
+            $error = MooseX::Exception::X::TryCatch->new( 
+                error   => $error,
+            );
+        }
+        foreach ($error) {
+            if (blessed $error) {
+                foreach my $catch_where (@catch_where) {
+                    if ($error->isa($catch_where->[0])) {
+                        _run_block($catch_where->[1],@finally);
+                        return;
+                    }
+                }
+            }
+            if ($catch_all) {
+                _run_block($catch_all);
+            }
+            _run_block(@finally);
+            return;
+        }
+    }
+    
+    _run_block(@finally);
+    return $wantarray ? @ret : $ret[0];
 }
 
 sub catch(&;@) {
@@ -88,6 +124,16 @@ sub where($;@) {
         @rest,
     );
 }
+
+sub _run_block {
+    my (@code_blocks) = @_;
+    
+    foreach my $code (@code_blocks) {
+        $code->();
+    }
+}
+
+1;
 
 
 #sub try (&;@) {
