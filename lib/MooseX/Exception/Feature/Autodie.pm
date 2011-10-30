247a71b6c7c159a3248b01516f7c64b4af176303
Maros Kollar <maros@k-1.com>
2011-10-30 15:54:36 +0100

    Working autodie exception handling

--- lib/MooseX/Exception/Feature/Autodie.pm
+++ lib/MooseX/Exception/Feature/Autodie.pm
@@ -3,4 +3,22 @@ package MooseX::Exception::Feature::Autodie;
 # ============================================================================
 use utf8;
 
+use strict;
+use warnings;
+
+use MooseX::Exception::Autodie;
+
+use autodie ();
+our @ISA = qw(autodie);
+
+sub exception_class { return "MooseX::Exception::Autodie" };
+
+sub import {
+    goto &autodie::import;
+}
+
+sub unimport {
+    goto &autodie::unimport;
+}
+
 1;
